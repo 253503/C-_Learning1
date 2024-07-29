f720f5ff68e70d412746d66a21e02924ea123dae
253503 <gayathri.mohandoss@ust.com>
2024-07-29 20:34:14 +0530

    Add files via upload

---
+++ ConsoleApp12/ConsoleApp12/operatoroverloadadd.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+
+    class Sample1
+    {
+        int Number1, Number2;
+        public Sample1(int a, int b)
+        {
+            Number1 = a; Number2 = b;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Number 1 :" + Number1 + " \n Number 2: " + Number2);
+        }
+
+        public static Sample1 operator +(Sample1 s1, Sample1 s2)
+        {
+            Sample1 s3 = new Sample1(0, 0);
+            s3.Number1 = s1.Number1 + s2.Number1;
+            s3.Number2 = s1.Number2 + s2.Number2;
+            return s3;
+
+        }
+    }
+    internal class OpeartorOverloadadd
+    {
+        public static void Main2()
+        {
+            Sample1 s1 = new Sample1(1, 2);
+            Sample1 s2 = new Sample1(2, 4);
+            Sample1 s3 = s1 + s2;
+            s3.Print();
+
+        }
+    }
+
+}
---
+++ ConsoleApp12/ConsoleApp12/overriding.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    
+        class baseClass
+        {
+            public baseClass()
+            {
+
+            }
+            public virtual void show()
+            {
+                Console.WriteLine("Base class");
+            }
+        }
+        class derivedClass : baseClass
+        {
+            public derivedClass() : base()
+            {
+
+            }
+            public override void show()
+            {
+                base.show();
+                Console.WriteLine("Derived class");
+            }
+        }
+        internal class Overrirding
+        {
+            public static void Main()
+            {
+                baseClass obj = new baseClass();
+                obj.show();
+
+                obj = new derivedClass();
+                obj.show();
+            }
+        }
+   
+}
