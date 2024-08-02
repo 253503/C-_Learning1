9068e87a1f6a8ba1b73270f57da99ed47104444b
253503 <gayathri.mohandoss@ust.com>
2024-08-02 17:49:59 +0530

    Day8
    added

---
+++ ConsoleApp14/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            CalculatorClient obj = new CalculatorClient();
+            Console.WriteLine("Sum from another assembly");
+            Console.WriteLine(obj.Sum(1, 2));
+        }
+    }
+}
