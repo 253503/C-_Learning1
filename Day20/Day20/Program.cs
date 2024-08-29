19c5af40d3c15a58046fc2968676d9e4b6b52e53
253503 <gayathri.mohandoss@ust.com>
2024-08-29 15:34:39 +0530

    SQLDB
    added

---
+++ Day20/Day20/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SqlClient;
+
+namespace DbConnectionDemo
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            SqlConnection con = new
+                SqlConnection("Data Source=8516181D2C415F4;Initial Catalog=master;Integrated Security=True;");
+            SqlCommand cmd = new SqlCommand("select * from tblEmployee", con);
+            con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                Console.WriteLine(rdr["id"] + " | " + rdr["EmployeeName"]);
+            }
+        }
+    }
+}
